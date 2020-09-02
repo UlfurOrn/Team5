879f6e1bc3c027776c49d8c6ff5f19a9bcaea21c
bjartur20 <bjartur20@gmail.com>
2020-09-02 15:10:37 +0000

    Created record DTO and Type dto

---
+++ API/app/main/util/DTO/recordDTO.py
@@ -0,0 +1,10 @@
+from flask_restplus import Namespace, fields
+
+class RecordDTO:
+    api = Namespace('record', description='Record related operations')
+    record = api.model('record', {
+        'UserId': fields.Integer(required=True, description='The user identification number for the record'),
+        'TypeId': fields.Integer(required=True, description='The type identifier for the record'),
+        'RDateTime': fields.DateTime(required=True, description='The tiestapm for the record'),
+        'Amount': field.Float(required=True, description='The amount for the record')
+    })
---
+++ API/app/main/util/DTO/typeDTO.py
@@ -0,0 +1,9 @@
+from flask_restplus import Namespace, fields
+
+class TypeDTO:
+    api = Namespace('type', description='Type related operations')
+    record = api.model('type', {
+        'Name': fields.String(required=True, description='The name of the type'),
+        'Description': fields.String(required=True, description='Short description for the type'),
+        'Measurement': fields.String('Measurement for the amount of a record of this type')
+    })
