8da2bc2f198c20e40eec06e9117842468bb9e9d3
palli <palli720@gmail.com>
2020-09-03 23:25:11 +0000

    TypeController

--- API/app.py
+++ API/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restplus import Api
 
 
+
 app = Flask(__name__)
 api = Api(app)
 
---
+++ API/app/main/controler/typeController.py
@@ -0,0 +1,37 @@
+from flask import request
+from flask_restplus import Resource
+from ..services.dbapi import DBapi
+
+from ..util.DTO.typeDTO import TypeDTO
+
+api = TypeDTO.api
+
+@api.route('/type')
+class TypeList(Resource):
+    @api.doc('List all types')
+    def get(self):
+        return DBapi.types('GET')
+
+    @api.response(201, 'Type successfully created.')
+    @api.doc('create a new Type')
+    @api.expect(_user, validate=True)
+    def post(self):
+        data = request.json
+        return DBapi.types('POST',data=data)
+
+@api.route('/type/<id>')
+@api.response(404, 'Type not found.')
+class Type(Resource):
+    @api.doc('Get a single type')
+    def get(self):
+        return DBapi.('GET',id)
+
+    @api.doc('Edit a type')
+    def put(self):
+        data = request.json
+        return DBapi('PUT',id,data)
+
+    @api.doc('Delete a type')
+    def delete(self):
+        return DBapi('DELETE',id)
+    
