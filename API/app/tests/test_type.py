f264812cf800eec6d3aa7ceda02016f8323a15ba
bjartur20 <bjartur20@gmail.com>
2020-09-05 16:21:54 +0000

    Added skeleton for tests

---
+++ API/app/tests/test_type.py
@@ -0,0 +1,16 @@
+from app.main.controller.services.dbapi import DBapi
+
+def test_get_single_type():
+    pass
+
+def test_get_type_list():
+    pass
+
+def test_post_type():
+    pass
+
+def test_put_type():
+    pass
+
+def test_delete_tyoe():
+    pass
