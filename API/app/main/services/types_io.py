1b86353424b9bce08f8eb4d55109439cd4f6825c
Ulfur_Orn <ulfurinn@gmail.com>
2020-09-05 18:43:17 +0000

    Renaming

--- API/app/main/services/types_io.py
+++ API/app/main/services/types_io.py
@@ -7,10 +7,10 @@ class Typesio(AbcTable):
         Contains methods for each CRUD operation [GET, POST, PUT, DELETE]
     """
     @classmethod
-    def get(cls, id):
+    def get(cls, type_id):
         """ Takes in an int. Returns row from types with set id or all rows if id=None """
         if id:
-            super()._cur.execute("SELECT * FROM types WHERE typeid = %s;", (id,))
+            super()._cur.execute("SELECT * FROM types WHERE typeid = %s;", (type_id,))
         else:
             super()._cur.execute("SELECT * FROM types;")
         return super()._cur.fetchall()
@@ -22,11 +22,11 @@ class Typesio(AbcTable):
         super()._cur.execute("INSERT INTO types (name, description, measurement) VALUES (%s, %s, %s);", (name, description, measurement))
 
     @classmethod
-    def put(cls, id, data):
+    def put(cls, type_id, data):
         """ Takes in an int and a dict with info to change and updates those columns in the database. Returns nothing """
         values = [val for val in data.values()] # Get all keys from the input dict
         keys = [key for key in data.keys()]     # Get all values from the input dict
-        values.extend([id])
+        values.extend([type_id])
 
         commandStr = "UPDATE types SET "
         for i in range(len(keys)):                      # Add all updates to string
@@ -37,6 +37,6 @@ class Typesio(AbcTable):
         super()._cur.execute(commandStr, values)
 
     @classmethod
-    def delete(cls, id):
+    def delete(cls, type_id):
         """ Takes in an int. Deletes row with that id from the database. Returns nothing """
-        super()._cur.execute("DELETE FROM types WHERE typeid = %s;", (id,))
+        super()._cur.execute("DELETE FROM types WHERE typeid = %s;", (type_id,))
--- API/app/main/services/users_io.py
+++ API/app/main/services/users_io.py
@@ -7,10 +7,10 @@ class Usersio(AbcTable):
         Contains methods for each CRUD operation [GET, POST, PUT, DELETE]
     """
     @classmethod
-    def get(cls, id):
+    def get(cls, user_id):
         """ Takes in an int. Returns row from users with set id or all rows if id=None """
-        if id:
-            super()._cur.execute("SELECT * FROM users WHERE userid = %s;", (id,))
+        if user_id:
+            super()._cur.execute("SELECT * FROM users WHERE userid = %s;", (user_id,))
         else:
             super()._cur.execute("SELECT * FROM users;")
         return super()._cur.fetchall()
@@ -18,16 +18,16 @@ class Usersio(AbcTable):
     @classmethod
     def post(cls, data):
         """ Takes in a dict with a user and saves to the database. Returns nothing """
-        name, email, dob, gender, weight, height = data["Name"], data["Email"], data["DoB"], data["Gender"], data["Weight"], data["Height"]
+        name, email, dob, gender, weight, height = data["name"], data["email"], data["dob"], data["gender"], data["weight"], data["height"]
         super()._cur.execute("INSERT INTO users (Name, Email, DoB, Gender, Weight, Height) VALUES (%s, %s, %s, %s, %s, %s);",
             (name, email, dob, gender, weight, height))
 
     @classmethod
-    def put(cls, id, data):
+    def put(cls, user_id, data):
         """ Takes in an int and a dict with info to change and updates those columns in the database. Returns nothing """
         values = [val for val in data.values()] # Get all keys from the input dict
         keys = [key for key in data.keys()]     # Get all values from the input dict
-        values.extend([id])
+        values.extend([user_id])
 
         commandStr = "UPDATE users SET "
         for i in range(len(keys)):                      # Add all updates to string
@@ -38,6 +38,6 @@ class Usersio(AbcTable):
         super()._cur.execute(commandStr, values)
 
     @classmethod
-    def delete(cls, id):
+    def delete(cls, user_id):
         """ Takes in an int. Deletes row with that id from the database. Returns nothing """
-        super()._cur.execute("DELETE FROM users WHERE userid = %s;", (id,))
+        super()._cur.execute("DELETE FROM users WHERE userid = %s;", (user_id,))
