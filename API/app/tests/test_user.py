13c3229fbd17ecd154cab1fa95a88a682b53e6a2
Your Name <magnuskonrad@gmail.com>
2020-09-06 17:10:40 +0000

    pull changes

--- API/app/tests/test_user.py
+++ API/app/tests/test_user.py
@@ -1,7 +1,20 @@
 from app.main.controller.services.dbapi import DBapi
 
-def test_get_single_user():
-    pass
+def test_get_single_user(mock_db):
+    test_user = {
+        'name': "TestUser",
+        'email': ""
+        'dob': ""
+        'gender': ""
+        'weight': ""
+        'height': ""
+    }
+
+    mock_db.return_value = test_type
+
+    data = requests.get(URL + "/get/1")
+    int = "k"
+    
 
 def test_get_user_list():
     pass
