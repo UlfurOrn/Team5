72a24649684e4507e17b40732cbe231034901848
Your Name <magnuskonrad@gmail.com>
2020-09-06 18:06:33 +0000

    2 tests added

--- API/app/tests/endpoints/test_user.py
+++ API/app/tests/endpoints/test_user.py
@@ -4,40 +4,75 @@ import psycopg2
 from psycopg2 import extras
 import pytest
 
+import requests
+from unittest.mock import patch
+
+from tests.endpoints.test_base import TestBase
+
 # This connects the AbcTable to another designated test database
 AbcTable._conn = psycopg2.connect("dbname=habittest user=habittester password=tester123  host=gudjoniv.com")
 AbcTable._conn.autocommit = True
 AbcTable._cur = AbcTable._conn.cursor(cursor_factory=extras.DictCursor)
 
+@patch("main.services.db_api.DBapi.users")
+class TestUserEndpoint(TestBase):
+    def test_get_single_user(self, mock_db):
+        test_user = {
+            'name': "testuser",
+            'email': "testuser@email.com",
+            'dob': "2020-04-25",
+            'gender': "m",
+            'weight': "85",
+            'height': "180"
+        }
 
-def test_get_single_user():    
-    test_user = {
-        'name': "TestUser",
-        'email': "",
-        'dob': "",
-        'gender': "",
-        'weight': "",
-        'height': ""
-    }
+        mock_db.return_value = [test_user]
 
-    mock_db.return_value = test_type
+        response = self.app.get("type/1")
+        data = response.json
 
-    data = requests.get(URL + "/get/1")
-    data = data.json
+        assert data == test_user
+        assert mock_db.called_once_with('GET', '1')
 
-    assert data == test_type
 
-def test_get_user_list():
-    pass
+    def test_get_user_list(self, mock_db):
+        test_user_list = [
+            {
+                'name': "testuser",
+                'email': "test@email.com",
+                'dob': "2020-04-25",
+                'gender': "m",
+                'weight': "85",
+                'height': "180"
+            },
+            {
+                'name': "testuser2",
+                'email': "test2@email.com",
+                'dob': "2000-01-25",
+                'gender': "f",
+                'weight': "60",
+                'height': "170"
+            }
+        ]
 
-def test_post_user():
-    pass
+        mock_db.return_value = test_user_list
 
-def test_put_user():
-    pass
+        response = self.app.get("/user")
+        data = response.json
 
-def test_delete_user():
-    pass
+        assert data == {
+            "users": test_user_list
+        }
+        assert mock_db.called_once_with('GET')
 
-def test_exceptions_user():
-    pass
+    def test_post_user(self):
+        pass
+
+    def test_put_user(self):
+        pass
+
+    def test_delete_user(self):
+        pass
+
+    def test_exceptions_user(self):
+        pass
