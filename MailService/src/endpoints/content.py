8fc5b4936f35e49aa5bc4b301644b58d8cf4f71c
Ulfur_Orn <ulfurinn@gmail.com>
2020-09-20 14:00:09 +0000

    Created unit tests for content endpoints

--- MailService/src/endpoints/content.py
+++ MailService/src/endpoints/content.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restplus import Resource
 
 from models.content_dto import ContentDTO
-# from mail_service import mail_service
+from mail_service import MailService
 
 api = ContentDTO.api
 content_model = ContentDTO.model
@@ -14,7 +14,8 @@ class ContentEndpoint(Resource):
     def get(self):
         """Get content of email"""
 
-        return # mail_service.get_content()
+        mail_service = MailService()
+        return mail_service.get_content()
 
     @api.expect(content_model, validate=True)
     @api.marshal_with(content_model)
@@ -24,5 +25,6 @@ class ContentEndpoint(Resource):
         data = request.json
         content = data["content"]
 
-        # mail_service.set_content(content)
-        return # mail_service.get_content()
+        mail_service = MailService()
+        mail_service.set_content(content)
+        return mail_service.get_content()
---
+++ MailService/src/tests/test_content.py
@@ -0,0 +1,29 @@
+
+from unittest.mock import patch
+
+from tests.test_base import TestBase
+from tests.mail_service_stub import MailServiceStub
+
+
+@patch("endpoints.content.MailService")
+class TestContent(TestBase):
+
+    def test_get_content(self, mock_mail_service):
+        mock_mail_service.return_value = MailServiceStub()
+
+        response = self.app.get("/content")
+        data = response.json
+
+        assert data["content"] == "content"
+
+    def test_put_content(self, mock_mail_service):
+        mock_mail_service.return_value = MailServiceStub()
+
+        test_content = {
+            "content": "Test Content"
+        }
+
+        response = self.app.put("/content", headers=self.valid_header, json=test_content)
+        data = response.json
+
+        assert data == test_content
