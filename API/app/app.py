52e96d9d7d86176ea2394912e11276d1ea12ce48
bjartur20 <bjartur20@gmail.com>
2020-09-20 10:17:03 +0000

    Renamed types to habits

--- API/app/app.py
+++ API/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restplus import Api
 
 from main.controller.record_controller import api as record_ns
-from main.controller.type_controller import api as type_ns
+from main.controller.habit_controller import api as habit_ns
 from main.controller.user_controller import api as user_ns
 from main.controller.health_controller import api as health_ns
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 api = Api()
 
 api.add_namespace(record_ns)
-api.add_namespace(type_ns)
+api.add_namespace(habit_ns)
 api.add_namespace(user_ns)
 api.add_namespace(health_ns)
 
