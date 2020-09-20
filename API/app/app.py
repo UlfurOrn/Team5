3c2fde7fd70b8a3dca0c5fbc58d92c1be87a54a2
bjartur20 <bjartur20@gmail.com>
2020-09-20 09:42:20 +0000

    Health endpoint added

--- API/app/app.py
+++ API/app/app.py
@@ -4,6 +4,7 @@ from flask_restplus import Api
 from main.controller.record_controller import api as record_ns
 from main.controller.type_controller import api as type_ns
 from main.controller.user_controller import api as user_ns
+from main.controller.health_controller import api as health_ns
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ api = Api()
 api.add_namespace(record_ns)
 api.add_namespace(type_ns)
 api.add_namespace(user_ns)
+api.add_namespace(health_ns)
 
 api.init_app(app)
 
---
+++ API/app/main/controller/health_controller.py
@@ -0,0 +1,12 @@
+from flask import request
+from flask_restplus import Resource, Namespace
+
+api = Namespace('health', description='API health checker')
+
+@api.route('')
+class health(Resource):
+    @api.doc('Get the health of the API')
+    def get(self):
+        resp = {'message': 'Healthy'}
+        return resp
+        
