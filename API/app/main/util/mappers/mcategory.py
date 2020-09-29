22b34bc04c266e8393b99a1987e521664cce15e3
Ulfur_Orn <ulfurinn@gmail.com>
2020-09-29 12:41:27 +0000

    Refactor category mapper

--- API/app/main/util/mappers/mcategory.py
+++ API/app/main/util/mappers/mcategory.py
@@ -1,9 +1,8 @@
 from main.util.mappers.mapper import Mapper
 
+
 class Mcategory(Mapper):
     """
         A Mapper class for the MCategories table
     """
-    def __init__(self, mcategoryid=None, name=None):
-        self.mcategoryid = mcategoryid
-        self.name = name
+    KEYS = ["mcategoryid", "name"]
