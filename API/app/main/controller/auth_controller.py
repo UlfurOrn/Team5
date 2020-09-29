98d1e8a3df45ad9bce4411d3c049ecfddf99b7ad
Ulfur_Orn <ulfurinn@gmail.com>
2020-09-29 14:55:33 +0000

    Linting in controllers

--- API/app/main/controller/auth_controller.py
+++ API/app/main/controller/auth_controller.py
@@ -1,19 +1,19 @@
 from flask import request
-from flask_restplus import Resource, Namespace
+from flask_restplus import Resource
 
 from main.services.db_api import DBapi
-# from main.repositories.users_io import Usersio
 from main.util.DTO.auth_dto import AuthDTO
 
 api = AuthDTO.api
 _auth = AuthDTO.auth
+
 
 @api.route('/login')
 class UserLogin(Resource):
     @api.doc('User login')
     @api.expect(_auth, validate=True)
     def post(self):
-        '''Uses the Userio.password() function to check if the 
+        '''Uses the Userio.password() function to check if the
         username and password match.
 
         Returns:
@@ -27,4 +27,3 @@ class UserLogin(Resource):
             return "successfully logged in", 200
         else:
             return "couldn't log in", 404
-        
