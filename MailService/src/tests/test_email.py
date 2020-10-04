c54cff299dda3f3d264095a0f95ee10c498e5e16
Ulfur_Orn <ulfurinn@gmail.com>
2020-10-04 14:39:43 +0000

    Switch to mail service stub in test base

--- MailService/src/tests/test_email.py
+++ MailService/src/tests/test_email.py
@@ -2,24 +2,18 @@
 from unittest.mock import patch
 
 from tests.test_base import TestBase
-from mail_service.mail_service_stub import MailServiceStub
 
 
-@patch("endpoints.email.MailService")
 class TestSubject(TestBase):
 
-    def test_get_email(self, mock_mail_service):
-        mock_mail_service.return_value = MailServiceStub()
-
+    def test_get_email(self):
         response = self.app.get("/email")
         data = response.json
 
         assert data["subject"] == "subject"
         assert data["content"] == "content"
 
-    def test_post_email(self, mock_mail_service):
-        mock_mail_service.return_value = MailServiceStub()
-
+    def test_post_email(self):
         test_emails = {
             "emails": [
                 "test1@email.com",
