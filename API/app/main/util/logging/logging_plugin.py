3102a39650b2a6b4eeb59840239bead81cfbaf18
Ulfur_Orn <ulfurinn@gmail.com>
2020-10-04 15:17:42 +0000

    Started implementation of different logging

--- API/app/main/util/logging/logging_plugin.py
+++ API/app/main/util/logging/logging_plugin.py
@@ -4,7 +4,8 @@ from logging.config import fileConfig
 
 class LoggingPlugin:
 
-    LOGGING_CONFIG = "main/util/logging/logging.ini"
+    LOGGING_LIVE_CONFIG = "main/util/logging/live_logging.ini"
+    LOGGING_DEV_CONFIG = "main/util/logging/dev_logging.ini"
 
     @classmethod
     def get_logger(cls):
