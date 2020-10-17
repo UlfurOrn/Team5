7bfcacee89ca970b936c5c22c5168add00f8ca91
Ulfur_Orn <ulfurinn@gmail.com>
2020-10-17 16:18:12 +0000

    Finished category controller

--- API/app/main/controller/category_controller.py
+++ API/app/main/controller/category_controller.py
@@ -1,36 +1,36 @@
 from flask import request
 from flask_restplus import Resource
 
-from main.util.mappers.categorymapper import McategoryMapper
+from main.util.mappers.categorymapper import CategoryMapper
 from main.util.DTO.category_dto import CategoryDTO
 from main.services.db_api import DBapi
 
 api = McategoryDTO.api
-_measurement = MeasurementDTO.model
+_category = CategoryDTO.model
 
 
 @api.route('')
-class MeasurementList(Resource):
-    @api.doc('List all measurements')
-    @api.marshal_list_with(_measurement, envelope='measurements')
+class CategoryList(Resource):
+    @api.doc('List all categories')
+    @api.marshal_list_with(_category, envelope='categories')
     def get(self):
-        data = DBapi.measurements.get()
+        data = DBapi.mcategories.get()
 
-        measurement_list = []
-        for measurement in data:
-            measurement_list.append(measurement.to_dict())
+        category_list = []
+        for category in data:
+            category_list.append(category.to_dict())
 
-        return measurement_list
+        return category_list
 
 
-@api.route('/<measurement_id>')
-@api.response(404, 'Measurement not found.')
-class SingleMeasurement(Resource):
-    @api.doc('Get a single measurement')
-    @api.marshal_with(_measurement)
-    def get(self, measurement_id):
-        data = DBapi.measurements.get(measurement_id)
+@api.route('/<category_id>')
+@api.response(404, 'Category not found.')
+class SingleCategory(Resource):
+    @api.doc('Get a single category')
+    @api.marshal_with(_category)
+    def get(self, category_id):
+        data = DBapi.mcategories.get(category_id)
         if not data:
             return "", 404
-        measurement_dict = data[0].to_dict()
-        return measurement_dict
+        category_dict = data[0].to_dict()
+        return category_dict
