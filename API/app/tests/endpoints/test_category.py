bffd74a2fdfcee0af0616d9efa5be859737ba403
Ulfur_Orn <ulfurinn@gmail.com>
2020-10-18 22:03:30 +0000

    Unit test category exceptions

--- API/app/tests/endpoints/test_category.py
+++ API/app/tests/endpoints/test_category.py
@@ -53,3 +53,17 @@ class TestCategoryEndpoint(TestBase):
             "categories": test_category_list
         }
         mock_db.assert_called_once()
+
+    def test_bad_request_exception(self):
+        response = self.app.get("category/0", headers=self.valid_header)
+
+        assert response.json["message"] == "Category id must be higher than 0"
+        assert response.status_code == 400
+
+    @patch("main.controller.category_controller.DBapi.mcategories.get")
+    def test_not_found_exception(self, mock_get):
+        mock_get.return_value = False
+        response = self.app.get("category/1", headers=self.valid_header)
+
+        assert response.json["message"] == "Category with id 1 not found"
+        assert response.status_code == 404
